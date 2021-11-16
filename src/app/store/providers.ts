9b639a3a3c549946103f1920ef77ab33747b4642
Jesús Gómez <jesgombel@outlook.es>
2021-11-16 20:10:42 +0100

    refactor(store): initial value like variable

--- src/app/store/providers.ts
+++ src/app/store/providers.ts
@@ -1,7 +1,10 @@
+import { Provider } from '../api/justwatch.types'
 import { Store } from './store.types'
 
+const initialValue: Provider['id'][] = []
+
 export function providers (store: Store) {
-  store.on('@init', () => ({ providers: [] }))
+  store.on('@init', () => ({ providers: initialValue }))
 
   store.on('@providers/set', (_, providers) => {
     return { providers }
